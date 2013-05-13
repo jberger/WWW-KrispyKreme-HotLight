926945183485c25fc7d7a603a8bc47e193bdbb29
Curtis Brandt <curtisjbrandt@gmail.com>
2013-05-13 16:48:48 -0700

    start work on library...not perfect

--- lib/WWW/KrispyKreme/Hotlight.pm
+++ lib/WWW/KrispyKreme/Hotlight.pm
@@ -1,8 +1,80 @@
 package WWW::KrispyKreme::Hotlight;
 
-use strict;
+use Moose;
 use 5.008_005;
+use URI;
+use WWW::Mechanize;
+use HTML::TreeBuilder::XPath;
+use Geo::PostalCode;
+
 our $VERSION = '0.01';
+
+has where => (is => 'rw', isa => 'Int');
+
+has hours => (is => 'rw', lazy => 1, builder => '_build_hours');
+
+sub _build_hours {
+    my ($self) = @_;
+    my $loc = $self->{where} or return {};
+
+    #my %location_hours;
+
+    my $mech = WWW::Mechanize->new;
+    my $uri = URI->new('http://locations.krispykreme.com/');
+    $mech->get($uri->as_string);
+    my ($form) = $mech->forms;
+    foreach my $input ($form->inputs) {
+        $input->value($loc) if $input->name and $input->name eq 'PostalCode';
+    }
+    #warn $form->attr('id');
+    my $r = $mech->submit_form(with_fields => {PostalCode => 91202});
+    warn $r->is_success;
+    warn $r->content;
+    #$uri->query_form({PostalCode => $loc,});
+    #$mech->post
+
+    #my $tree = HTML::TreeBuilder::XPath->new_from_content($mech->content);
+    #my @cities =
+    #  map { my $h = $_->attr('href'); $h =~ /^http/ ? $h : "http://locations.krispykreme.com$h" }
+    #  grep { $_->as_text =~ /$loc/ }
+    #  $tree->findnodes('.//ul[@class="content-states"]//ul/li/a[@href=~/location/]');
+
+    #foreach my $city (@cities) {
+    #    $mech->get($city);
+    #    $tree->parse_content($mech->content);
+
+    #    my @hrs = map { split(/\s*\|\s*/, $_) } $tree->findvalues('.//ul[@class="hours-container"]//li[not(./*)]');
+    #    $location_hours{$city} = \@hrs;
+
+    #    if (@hrs) {
+    #        my $parsed = $self->_parse_hours(join '; ', @hrs);
+    #        use Data::Dumper;
+    #        warn Dumper $parsed;
+    #    }
+    #}
+    ## '5:30-10A & 5:30-9:30 P (Sun-Thr)',
+    ## '5:30 A-10A & 5-10P Fri & Sat'
+    #return \%location_hours;
+}
+
+sub _parse_hours {
+    my ($self, $hours) = @_;
+
+    my %hrs;
+    warn $hours;
+
+    my $hr_regex = qr{\d{1,2}[:\d]* \s* (?:to|-) \s* \d{1,2}[:\d]*}ix;
+    warn $hours =~ /$hr_regex{2}/;# (?: (?:and|&) \d{1,2})?
+    #/ix;
+    #(?: \s+ \(? ([A-Z]{3,}) \s* [\-&] \s* ([A-Z]{3,}) \)? )? \b
+
+    #while ($hours =~ /(\d{1,2}[:\d]*)\s*(?:to|-)?\s*(?:\s+\(?([A-Z]{3,})\s*[\-&]\s*([A-Z]{3,})\)?)?\b/gi) {
+    #    warn $1;
+    #    warn $2;
+    #}
+
+    return \%hrs;
+}
 
 1;
 __END__
